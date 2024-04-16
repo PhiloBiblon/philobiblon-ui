3cfd235c7c89b6fabb132f38b190a257034494e7
jmformenti <jmformenti@gmail.com>
2024-04-16 22:03:26 +0200

    feat: #64 Allow show html markup (#67)

--- frontend/components/item/util/ViewTextLang.vue
+++ frontend/components/item/util/ViewTextLang.vue
@@ -1,6 +1,7 @@
 <template>
   <span v-if="value && value.value">
-    {{ value.value }}
+    <!-- eslint-disable-next-line vue/no-v-html -->
+    <span v-html="value.value" />
     <sup v-if="value.showLanguage || (value.language && value.language != $i18n.locale)">{{ value.language }}</sup>
   </span>
 </template>
