7854a0d41bd5b62c64917530f70a94e9ca55fe1a
Austin Worthington <93830141+austininseoul@users.noreply.github.com>
2024-04-23 18:12:50 +0900

    ui changes

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -3,7 +3,7 @@
     <v-navigation-drawer
       v-model="drawer"
       color="primary"
-      fixed
+      :permanent="!$vuetify.breakpoint.sm && !$vuetify.breakpoint.xs && !$vuetify.breakpoint.md"
       app
       dark
     >
@@ -20,7 +20,7 @@
         </v-list-item>
       </template>
 
-      <v-divider />
+      <v-divider/>
 
       <v-list class="mainmenu">
         <v-list-item :to="localePath('/')">
@@ -86,7 +86,6 @@
       fixed
       app
       dark
-      src="/img/header_page-opacity15_2.gif"
     >
       <template #img="{ props }">
         <v-img
@@ -94,7 +93,7 @@
           gradient="to top right, rgba(33, 33, 33,.7), rgba(250, 250, 250,.7)"
         />
       </template>
-      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-app-bar-nav-icon class="d-lg-none" @click.stop="drawer = !drawer"/>
       <v-toolbar-title
         class="text-h4"
         style="cursor: pointer"
@@ -102,8 +101,8 @@
       >
         {{ title_1 }}<span class="font-weight-light">{{ title_2 }}</span>
       </v-toolbar-title>
-      <v-spacer />
-      <languages-menu />
+      <v-spacer/>
+      <languages-menu/>
       <v-tooltip
         v-if="$store.state.auth.isLogged"
         bottom
@@ -133,23 +132,67 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer
-      app
-      :padless="true"
-    >
-      <v-card
-        flat
-        tile
-        width="100%"
-        class="text-center"
-      >
-        <v-card-text class="py-2 font-weight-light">
-          &copy; {{ new Date().getFullYear() }}
-          <span class="version">
-            v.{{ version }}
-          </span>
-        </v-card-text>
-      </v-card>
+    <v-footer color="white" class="pa-10 pb-8 z-index-100">
+      <div class="d-flex justify-space-between w-100">
+        <div class="d-flex justify-space-around footer-content w-100">
+          <div class="footer-col pt-3">
+            <div class="v-toolbar-title">
+              {{ title_1 }}<span class="font-weight-light">{{ title_2 }}</span>
+            </div>
+            <p class="text-center">
+              <a class="footer_a" href="https://update.lib.berkeley.edu/Topics/philobiblon/">
+                Read PhiloBlog
+              </a>
+            </p>
+          </div>
+          <v-list color="transparent" class="footer-col pa-0" dense>
+            <v-list-item v-for="(item, index) in links" :key="index" link>
+              <v-list-item-title class="link" @click="goTo(item.link)">{{ item.label }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+          <v-list color="transparent" class="footer-col pa-0" dense>
+            <v-list-item v-for="(item, index) in linksSecond" :key="index" link>
+              <v-list-item-title class="link" @click="goTo(item.link)">{{ item.label }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+          <div class="transparent-card footer-col">
+            <div class="d-flex mb-10">
+              <div class="d-flex py-2 pr-2 border-right align-center">
+                <a href="https://facebook.com/pages/PhiloBiblon/289959314407502" class="me-3">
+                  <IconsSocialFbIcon/>
+                </a>
+                <a href="https://twitter.com/PPhiloBiblon" class="me-3">
+                  <IconsSocialTwitterIcon/>
+                </a>
+                <a href="https://youtube.com/channel/UCOVWrLQiC1bPvt57pYQQeJQ/videos" class="me-3">
+                  <IconsSocialYoutubeIcon/>
+                </a>
+                <a href="https://instagram.com/p/B6Ihdigpv1G/" class="me-3">
+                  <IconsSocialInstagramIcon/>
+                </a>
+              </div>
+              <ul class="d-flex align-center py-2 px-3 cite">
+                <li class="hl__linked-list__item">
+                    <a class="footer_a" href="https://bancroft.berkeley.edu/philobiblon/citation_en.html">
+                      How to cite {{ title_1 }}{{ title_2 }}
+                    </a>
+                </li>
+              </ul>
+            </div>
+            <div class="d-flex align-center mb-4">
+              <p class="mb-0 footer-desc">
+                Volume 2024, Number 3 (March) ISSN 1096-6609
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="d-flex justify-space-between align-center pt-8 w-100">
+        <img src="/img/footer/universitat_barcelona.png" alt="universitat_barcelona">
+        <img src="/img/footer/upf.png" alt="upf">
+        <img src="/img/footer/the_bancroft.png" alt="the_bancroft">
+        <img src="/img/footer/ilf.png" alt="ilf">
+      </div>
     </v-footer>
   </v-app>
 </template>
@@ -162,7 +205,20 @@ export default {
       title_1: 'Philo',
       title_2: 'Biblon',
       version: '0.6.0',
-      drawer: false
+      drawer: false,
+      links: [
+        {label: 'SEARCH', link: '/search_en'},
+        {label: 'ABOUT PhiloBiblon', link: '/about_en'},
+        {label: 'HELP', link: '/help_enl'},
+        {label: 'RESOURCES', link: '/resources_en'},
+        {label: 'COLLABORATE', link: '/collaborate_en'},
+      ],
+      linksSecond: [
+        {label: 'BETA', link: '/beta'},
+        {label: 'BIPA', link: '/bipa'},
+        {label: 'BITAGAP', link: '/bitagap'},
+        {label: 'BITECA', link: '/biteca_en'},
+      ],
     }
   },
   computed: {
@@ -209,11 +265,93 @@ export default {
 .mainmenu >>> .v-list-item--active {
   color: white;
 }
-.version {
-  float: right;
-  margin-right: 20px;
-}
 .subitem {
   padding-left: 35%;
+}
+.link {
+  color: black !important;
+  font-size: 14px !important;
+  font-weight: 400 !important;
+  line-height: 27px !important;
+  letter-spacing: 1.87px !important;
+  text-transform: uppercase !important;
+}
+.w-100 {
+  width: 100%;
+}
+.z-index-100 {
+  z-index: 100;
+}
+.v-toolbar-title {
+  cursor: pointer;
+  color: black;
+  font-size: 40px;
+  font-weight: bold;
+
+  > span {
+    font-weight: semibold;
+    color: #b71c1c;
+  }
+}
+.transparent-card {
+  background-color: transparent !important;
+}
+.border-right {
+  border-right: 1px solid gray;
+}
+.footer_a {
+  text-decoration: none;
+}
+.footer_a:hover {
+  border-bottom: 1px solid white;
+}
+.cite {
+  list-style: none;
+  gap: 34px;
+}
+.footer-desc {
+  color: black;
+  padding-left: 10px;
+}
+@media (max-width: 1290px) and (min-width: 1150px) {
+  .link {
+    font-size: 12px !important
+  }
+}
+@media (max-width: 1150.98px) and (min-width: 991px) {
+  .link {
+    font-size: 10px !important
+  }
+}
+@media (max-width: 991px) {
+  .v-footer--fixed {
+    position: relative!important;
+  }
+}
+
+@media (max-width: 1263px) {
+  footer {
+    z-index: unset!important;
+  }
+}
+
+@media (min-width: 691.98px) and (max-width: 990.98px) {
+  .footer-col {
+    width: 45%;
+  }
+  .footer-content {
+    flex-wrap: wrap;
+    gap: 30px;
+  }
+}
+
+@media (max-width: 691.98px) {
+  .footer-col {
+    width: 100%;
+  }
+  .footer-content {
+    flex-wrap: wrap;
+    gap: 30px;
+  }
 }
 </style>
