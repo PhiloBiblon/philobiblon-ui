6357a0cb876ef14e7ab46c55de3a4a04cae2e257
Josep Maria Formentí Serra <jmformenti@gmail.com>
2024-09-07 13:22:41 +0200

    feat:#77 Add search filters for MsEd option

--- frontend/lang/pt.js
+++ frontend/lang/pt.js
@@ -149,7 +149,7 @@ export default {
           hint: 'Pesquise por qualquer um dos nomes formais ou comummente utilizados da biblioteca (por exemplo, no BETA, pesquise por Real Biblioteca, Biblioteca de Palacio ou simplesmente Palacio).'
         },
         call_number: {
-          label: 'Número de telefone',
+          label: 'Posição de stock',
           hint: ''
         }
       },
@@ -245,10 +245,6 @@ export default {
         library: {
           label: 'Biblioteca',
           hint: 'Search by the current or former name of the library that holds the manuscript or printed edition.'
-        },
-        shelfmark: {
-          label: 'Cota',
-          hint: 'Search for a current or former shelfmark in the holding library as well as for the shelfmark of a previous owner. Searches are not case-sensitive, e.g., “Inc. 1484” or “inc. 1484.”'
         },
         date: {
           label: 'Data',
