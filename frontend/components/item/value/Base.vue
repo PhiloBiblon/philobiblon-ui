4d50c9ca2f38b5bc7576f1f613be37aa910588e9
Josep Maria Formentí Serra <jmformenti@gmail.com>
2024-10-07 21:53:24 +0200

    rollback base value

--- frontend/components/item/value/Base.vue
+++ frontend/components/item/value/Base.vue
@@ -1,25 +1,6 @@
 <template>
   <div v-if="valueToView">
-    <v-tooltip v-if="truncated" top open-delay="200">
-      <template #activator="{ on, attrs }">
-        <div v-bind="attrs" v-on="on">
-          <component
-            :is="'item-value-type-' + valueToView.type"
-            :is-user-logged="isUserLogged"
-            :value-to-view="valueToView"
-            :save="editValue"
-          />
-        </div>
-      </template>
-      <span>{{ longValue }}</span>
-    </v-tooltip>
-    <component
-      :is="'item-value-type-' + valueToView.type"
-      v-else
-      :is-user-logged="isUserLogged"
-      :value-to-view="valueToView"
-      :save="editValue"
-    />
+    <component :is="'item-value-type-' + valueToView.type" :is-user-logged="isUserLogged" :value-to-view="valueToView" :save="editValue" />
   </div>
 </template>
 
@@ -37,48 +18,25 @@ export default {
     type: {
       type: String,
       default: null
-    },
-    inTable: {
-      type: Boolean,
-      default: false
-    },
-    columnWidth: {
-      type: String,
-      default: '200px'
     }
   },
   data () {
     return {
-      valueToView: null,
-      longValue: null,
-      truncated: false
+      valueToView: null
     }
   },
   computed: {
     isUserLogged () {
       return this.$store.state.auth.isLogged
-    },
-    maxTextLength () {
-      const width = parseInt(this.columnWidth, 10)
-      return Math.floor(width / 8)
     }
   },
   async mounted () {
-    const vtW = await this.$wikibase.getWbValue(
+    this.valueToView = await this.$wikibase.getWbValue(
       this.value.property,
       this.value.datatype,
       this.value.datavalue.value,
       this.$i18n.locale
     )
-    this.longValue = vtW.value
-
-    if (this.inTable && vtW.value.length > this.maxTextLength && !this.isUserLogged) {
-      this.truncated = true
-
-      vtW.value = `${vtW.value.slice(0, this.maxTextLength)}...`
-    }
-
-    this.valueToView = vtW
   },
   methods: {
     editValue (editableData) {
