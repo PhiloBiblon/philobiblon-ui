ee5bcf96d12beae54c8e7771866b5daee0bf482c
Josep Maria Formentí Serra <jmformenti@gmail.com>
2024-11-16 22:45:56 +0100

    feat:#3 Move current PhiloBiblon's HTMLs to here

--- frontend/plugins/dompurify.js
+++ frontend/plugins/dompurify.js
@@ -4,8 +4,8 @@ import DOMPurify from 'dompurify'
 export default ({ app }, inject) => {
   inject('sanitize', (content) => {
     return DOMPurify.sanitize(content, {
-      ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'a', 'img', 'br', 'p', 'h1', 'ul', 'li', 'table', 'tbody', 'tr', 'td'],
-      ALLOWED_ATTR: ['href', 'src', 'alt', 'class', 'id']
+      ALLOWED_TAGS: ['b', 'i', 'em', 'strong', 'a', 'img', 'br', 'p', 'h1', 'h2', 'ul', 'li', 'table', 'tbody', 'tr', 'td', 'blockquote'],
+      ALLOWED_ATTR: ['href', 'src', 'alt', 'class', 'id', 'style']
     })
   })
 }
