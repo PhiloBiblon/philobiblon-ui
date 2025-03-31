b10e88c5a03c7eb9ede54e6e7a5b6fee11b13621
pi <jmformenti@gmail.com>
2025-03-31 06:06:59 +0000

    Bumped frontend to 0.8.11

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -15,7 +15,7 @@ export default {
   },
 
   publicRuntimeConfig: {
-    version: '0.8.7',
+    version: '0.8.11',
     apiBaseUrl: process.env.API_BASE_URL || '/.',
     wikibaseBaseUrl: process.env.WIKIBASE_BASE_URL,
     sparqlBaseUrl: process.env.SPARQL_BASE_URL,
