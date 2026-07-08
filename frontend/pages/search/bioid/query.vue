02a850f898b8100c727f35ceb36dec0020cc3e89
NokkelDag <desiremopo@gmail.com>
2026-07-08 10:16:26 +0200

    Added extra reciprocal properties

--- frontend/pages/search/bioid/query.vue
+++ frontend/pages/search/bioid/query.vue
@@ -312,7 +312,7 @@ const form = {
                     ?item wdt:P476 ?pbid .
                     FILTER regex(?pbid, '{{database}} {{table}} ') .
                     {{bitagapGroupFilter}}
-                    VALUES ?property { wdt:P703 wdt:P141 wdt:P142 wdt:P150 wdt:P203 wdt:P84 wdt:P505 wdt:P629 wdt:P504 wdt:P258 wdt:P192 wdt:P191 wdt:P33 wdt:P161 wdt:P190 wdt:P220 wdt:P735 }
+                    VALUES ?property { wdt:P703 wdt:P141 wdt:P142 wdt:P150 wdt:P203 wdt:P84 wdt:P505 wdt:P629 wdt:P504 wdt:P258 wdt:P192 wdt:P191 wdt:P33 wdt:P161 wdt:P190 wdt:P220 wdt:P735 wdt:P257 wdt:P486 wdt:P591 }
                     ?item ?property ?target_item .
                   }
                 }
--- frontend/service/query.service.js
+++ frontend/service/query.service.js
@@ -575,7 +575,7 @@ export class QueryService {
     if (form.input.associated_person && form.input.associated_person.value && form.input.associated_person.value.target_item) {
       filters +=
         `
-        VALUES ?prop_related_person { wdt:P703 wdt:P141 wdt:P142 wdt:P150 wdt:P203 wdt:P84 wdt:P505 wdt:P629 wdt:P504 wdt:P258 wdt:P192 wdt:P191 wdt:P33 wdt:P161 wdt:P190 wdt:P220 wdt:P735 }
+        VALUES ?prop_related_person { wdt:P703 wdt:P141 wdt:P142 wdt:P150 wdt:P203 wdt:P84 wdt:P505 wdt:P629 wdt:P504 wdt:P258 wdt:P192 wdt:P191 wdt:P33 wdt:P161 wdt:P190 wdt:P220 wdt:P735 wdt:P257 wdt:P486 wdt:P591 }
         ?item ?prop_related_person wd:${form.input.associated_person.value.target_item} .
         `
     }
